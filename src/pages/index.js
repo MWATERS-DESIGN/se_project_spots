58f60839042a875bbd995043701b139c97ba459d
Marquis Waters <marquis.dwaters7@gmail.com>
2026-01-27 17:39:31 -0600

    Spots Final-Submission 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,33 +8,6 @@ import {
   disableButton,
 } from "../scripts/validation.js";
 
-const initialCards = [
-  {
-    name: "Val Thorens",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
-  },
-  {
-    name: "Restaurant terrace",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
-  },
-  {
-    name: "An outdoor cafe",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
-  },
-  {
-    name: "A very long bridge, over the forest and through the trees",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
-  },
-  {
-    name: "Tunnel with morning light",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
-  },
-  {
-    name: "Mountain house",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
-  },
-];
-
 const api = new Api({
   baseUrl: "https://around-api.en.tripleten-services.com/v1",
   headers: {
@@ -219,19 +192,6 @@ modals.forEach((modal) => {
   });
 });
 
-document.addEventListener("keydown", (e) => {
-  if (e.key === "Escape") {
-    modals.forEach((modal) => {
-      if (
-        modal.classList.contains("modal") &&
-        modal.classList.contains("modal_is-opened")
-      ) {
-        closeModal(modal);
-      }
-    });
-  }
-});
-
 previewModalCloseBtn.addEventListener("click", () => {
   closeModal(previewModal);
 });
@@ -255,7 +215,6 @@ function handleEditProfileSubmit(evt) {
         [editNameInput, editDescriptionInput],
         settings
       );
-      evt.target.reset();
     })
     .catch((err) => {
       console.error(err);
